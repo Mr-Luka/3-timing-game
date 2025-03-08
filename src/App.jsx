ab456fc5b11b337b6d1d4822072a04119983d257
Luka <luka.luka.luka93@gmail.com>
2025-03-07 23:08:42 -0800

    timer challenge and App cleared

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,9 @@
-import Player from './components/Player.jsx';
-import TimerChallenge from './components/TimerChallenge.jsx';
+
 
 function App() {
   return (
     <>
-      <Player />
       <div id="challenges">
-        <TimerChallenge title='Easy' targetTime={1}/>
-        <TimerChallenge title='Not so Easy' targetTime={5}/>
-        <TimerChallenge title='Medium' targetTime={10}/>
-        <TimerChallenge title='Hard' targetTime={15}/>
-        <TimerChallenge title='Oh boy...' targetTime={23}/>
       </div>
     </>
   );
--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,55 +1,19 @@
-import {useState, useRef} from 'react';
 
-import ResultModal from './ResultModal';
-
-
-export default function TimerChallenge({title, targetTime}){
-    const timer = useRef();
-    const dialog = useRef();
-    const [timeRemaining, setTimeRemaining] = useState(targetTime * 1000);
-
-    const timerActive = timeRemaining > 0 && timeRemaining < targetTime * 1000;
-
-    if(timeRemaining <= 0) {
-        clearInterval(timer.current);
-        dialog.current.open()
-    }
-
-    function handleReset (){
-        setTimeRemaining(targetTime * 1000);
-    }
-
-
-    function handleStart(){
-        timer.current = setInterval(()=> {
-            setTimeRemaining(prevTime => prevTime - 10)
-        }, 10);
-    }
-
-    function handleStop(){
-        dialog.current.open()
-        clearInterval(timer.current)
-    }
-
+export default function TimerChallenge(){
+ 
     return (
         <>
-        <ResultModal 
-            targetTime={targetTime}
-            ref={dialog} 
-            onReset={handleReset}
-            timeRemaining={timeRemaining}
-            />
             <section className="challenge">
-                <h2>{title}</h2>
+                <h2>Title</h2>
                 <p className="challenge-time">
-                    {targetTime} second{targetTime > 1 ? 's' : ''}</p>
+                    1 second/s</p>
                 <p>
-                    <button onClick={timerActive ? handleStop : handleStart}>
-                        {timerActive ? 'Stop' : 'Start'} challenge
+                    <button>
+                        Start challenge
                     </button>
                 </p>
-                <p className={timerActive ? 'active' : undefined}>
-                    {timerActive ? 'Timer is running...' : 'Timer is inactive.'}
+                <p>
+                    Timer is inactive.
                 </p>
             </section>
         </>
