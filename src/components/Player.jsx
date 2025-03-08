04b253c2d3d4a6b6a9d11b7211c911204056bc99
Luka <luka.luka.luka93@gmail.com>
2025-03-07 23:04:28 -0800

    cleaning Player component

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,24 +1,13 @@
-import {useState, useRef} from 'react';
+
 
 export default function Player() {
-  const playerName = useRef();
-  const [playerInput, setPlayerInput] = useState('');
-
-  function handleClik(){
-    setPlayerInput(playerName.current.value);
-    playerName.current.value = '';
-  }
-
 
   return (
     <section id="player">
-      <h2>Welcome {playerInput ? playerInput : 'unknown entity'}</h2>
+      <h2>Welcome unknown entity</h2>
       <p>
-        <input 
-          ref={playerName}
-          type='text'
-          />
-        <button onClick={handleClik}>Set Name</button>
+        <input type='text'/>
+        <button>Set Name</button>
       </p>
     </section>
   );
