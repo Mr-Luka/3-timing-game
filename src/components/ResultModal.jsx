8cef94b4bb58467c93bde3ca26cc9e57f2548866
Luka <luka.luka.luka93@gmail.com>
2025-03-10 14:08:29 -0700

    finished Result modal and timer challenge , finished the project

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,20 +1,36 @@
+import {useRef, useImperativeHandle, forwardRef} from 'react';
+import {createPortal} from 'react-dom';
+
+const ResultModal = forwardRef( function ResultModal ({targetTime, onReset, timeRemaining}, ref){
+    const dialog = useRef();
+
+    useImperativeHandle(ref, ()=> {
+        return {
+            open() {
+                dialog.current.showModal()
+            }
+        }
+    });
+
+    const userLost = timeRemaining <= 0;
+    const formatedRemainingTime = (timeRemaining / 1000).toFixed(2);
+    const score = Math.round((1 - timeRemaining / (targetTime * 1000)) * 100);
 
 
-export default function ResultModal (){
-
-
-    return (
-        <dialog className='result-modal' >
-            <h2>You Lost!</h2>
-            
-            <p>The target time was <strong>XX seconds.</strong></p>
-            <p>You stopped the timer with <strong>X seconds left.</strong></p>
-            <form method="dialog">
+    return createPortal(
+        <dialog className='result-modal' ref={dialog} onClose={onReset}>
+           {userLost ? <h2>You Lost!</h2> : <h2>Your Score: {score}</h2>}
+            <p>The target time was <strong>{targetTime} seconds.</strong></p>
+            <p>You stopped the timer with <strong>{formatedRemainingTime} seconds left.</strong></p>
+            <form method="dialog" onSubmit={onReset}>
                 <button>Close</button>
             </form>
-        </dialog>
+        </dialog>,
+        document.getElementById('modal')
     )
-}
+});
+
+export default ResultModal;
 
 
 
--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,37 +1,51 @@
 import {useState, useRef} from 'react';
 
+import ResultModal from './ResultModal.jsx';
 
 
-export default function TimerChallenge({title, targetTime}){
+export default function TimerChallenge({title, targetTime, onReset}){
     const time = useRef();
-    const [timeExpired, setTimeExpired] = useState(false);
-    const [timerStarted, setTimerStarted] = useState(false);
+    const dialog = useRef()
+    const [ timeRemaining, setTimeRemaining ] = useState(targetTime * 1000);
+
+
+    const timerActive = timeRemaining > 0 && timeRemaining < targetTime * 1000;
+
+    if(timeRemaining <= 0) {
+        dialog.current.open();
+        clearInterval(time.current);
+    }
+
+    function handleReset(){
+        setTimeRemaining(targetTime * 1000);
+    }
 
     
     function handleStart(){
-        time.current = setTimeout(()=> {
-            setTimeExpired(true);
-        }, targetTime * 1000)
-        setTimerStarted(true);
+        time.current = setInterval(()=> {
+            setTimeRemaining(prevTime => prevTime - 10)
+        }, 10)
     }
 
     function handleStop(){
-        clearTimeout(time.current);
+        dialog.current.open();
+        clearInterval(time.current);
     }
  
     return (
         <>
+            <ResultModal targetTime={targetTime} ref={dialog} onReset={handleReset} timeRemaining={timeRemaining}/>
             <section className="challenge">
                 <h2>{title}</h2>
                 <p className="challenge-time">
                     1 second{targetTime > 1 ? 's' : ''}</p>
                 <p>
-                    <button onClick={timerStarted ? handleStop : handleStart}>
-                       {timerStarted ? 'Stop' : 'Start'} challenge
+                    <button onClick={timerActive ? handleStop : handleStart}>
+                       {timerActive ? 'Stop' : 'Start'} challenge
                     </button>
                 </p>
-                <p className={timerStarted ? 'active' : undefined}>
-                    { timerStarted ? 'Time is running...' : 'Timer is inactive.'}
+                <p className={timerActive ? 'active' : undefined}>
+                    { timerActive ? 'Time is running...' : 'Timer is inactive.'}
                 </p>
             </section>
         </>
